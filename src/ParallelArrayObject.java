a892a7512c220f6d6352e56aa6a98bcc1f929f9d
rafaelgdgs <rafagsique@hotmail.com>
2023-10-31 21:12:50 -0300

    refactor ParallelArrayObject

--- src/ParallelArrayObject.java
+++ src/ParallelArrayObject.java
@@ -4,27 +4,27 @@ public class ParallelArrayObject {
     private int total;
     private int grupo;
 
-    public void setId(int _id) {
-        id = _id;
-    }
-
     public int getId() {
         return id;
     }
 
-    public void setTotal(int _total) {
-        total = _total;
+    public void setId(int _id) {
+        id = _id;
     }
 
     public int getTotal() {
         return total;
     }
 
-    public void setGrupo(int _grupo) {
-        grupo = _grupo;
+    public void setTotal(int _total) {
+        total = _total;
     }
 
     public int getGrupo() {
         return grupo;
+    }
+
+    public void setGrupo(int _grupo) {
+        grupo = _grupo;
     }
 }
