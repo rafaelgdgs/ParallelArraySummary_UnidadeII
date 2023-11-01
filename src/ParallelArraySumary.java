475df22dfdd4ef92dc1eba64e16ae31e5303d3bf
rafaelgdgs <rafagsique@hotmail.com>
2023-10-31 21:25:04 -0300

    methods created

--- src/ParallelArraySumary.java
+++ src/ParallelArraySumary.java
@@ -3,4 +3,12 @@ import java.util.List;
 public class ParallelArraySumary {
     List<ParallelArrayObject> lista;
 
+    public void Carregamento(int N) {
+
+    }
+
+    public void Processamento(int T) {
+
+    }
+
 }
