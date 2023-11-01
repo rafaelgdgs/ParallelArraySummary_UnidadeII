92410927425b0854fa239ba127cddc689cc6ac81
rafaelgdgs <rafagsique@hotmail.com>
2023-10-31 21:09:38 -0300

    Implementation of ParallelArrayObject

---
+++ src/ParallelArrayObject.java
@@ -0,0 +1,30 @@
+public class ParallelArrayObject {
+
+    private int id;
+    private int total;
+    private int grupo;
+
+    public void setId(int _id) {
+        id = _id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setTotal(int _total) {
+        total = _total;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setGrupo(int _grupo) {
+        grupo = _grupo;
+    }
+
+    public int getGrupo() {
+        return grupo;
+    }
+}
