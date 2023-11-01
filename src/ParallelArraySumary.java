dbf0280a2b7211484f6fc327f7f2d8c28c6daaf0
rafaelgdgs <rafagsique@hotmail.com>
2023-10-31 22:08:08 -0300

    first implementation of Carregamento method

--- src/ParallelArraySumary.java
+++ src/ParallelArraySumary.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ParallelArraySumary {
-    List<ParallelArrayObject> lista;
+    public List<ParallelArrayObject> lista;
 
     public void Carregamento(int N) {
-
+        List<ParallelArrayObject> listaTemp = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < Math.pow(10.0, N); i++) {
+            ParallelArrayObject obj = new ParallelArrayObject(i, random.nextDouble(10.0), random.nextInt(6));
+            listaTemp.add(obj);
+        }
+        lista = listaTemp;
     }
 
     public void Processamento(int T) {
